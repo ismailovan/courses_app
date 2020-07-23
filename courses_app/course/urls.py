c1aa41ff2aee7d8a649d51c5d60ecb9b2bb3fad4
Ismailova Nazgul <44084651+ismailovan@users.noreply.github.com>
2020-07-23 17:32:51 +0600

    Update urls.py

--- courses_app/course/urls.py
+++ courses_app/course/urls.py
@@ -3,8 +3,8 @@ from course import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('course/', views.CourseList.as_view()),
-    path('course/<int:pk>/', views.CourseDetail.as_view()),
+    path('course/', views.CourseList.as_view(), name = 'courseList'),
+    path('course/<int:pk>/', views.CourseDetail.as_view(), name = 'courseDetail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
