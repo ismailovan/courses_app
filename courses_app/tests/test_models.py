3a6b170984d1bd614912f0937eb930a78a1f226d
Nazgul <ismailova_n@auca.kg>
2020-07-26 21:45:26 +0600

    edit spaces

--- courses_app/tests/test_models.py
+++ courses_app/tests/test_models.py
@@ -6,21 +6,21 @@ from contact.models import Contact
 
 class CourseTestClass(TestCase):
 
-	@classmethod
-	def setUpTestData(cls):
-		category = Category.objects.create(name = "Test category", imgpath = "Test image")
-		course = Course.objects.create(name = "Test name", description = "Test description", logo = "Test logo", category = category)
-		contact = Contact.objects.create(contact_type = 1, value = "Test value", course = course)
-		branch = Branch.objects.create(latitude = "Test latitude", longtitude = "Test longtitude", adress = "Test adress", course = course)
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name = "Test category", imgpath = "Test image")
+        course = Course.objects.create(name = "Test name", description = "Test description", logo = "Test logo", category = category)
+        contact = Contact.objects.create(contact_type = 1, value = "Test value", course = course)
+        branch = Branch.objects.create(latitude = "Test latitude", longtitude = "Test longtitude", adress = "Test adress", course = course)
 	
-	def test_category(self):
-		category = Category.objects.get(id = 1)
+    def test_category(self):
+        category = Category.objects.get(id = 1)
 		field_label_name = category.name
 		field_label_image = category.imgpath
 		self.assertEquals(field_label_name, "Test category")
 		self.assertEquals(field_label_image, "Test image")
 
-	def test_course(self):
+    def test_course(self):
 		course = Course.objects.get(id = 1)
 		field_label_name = course.name
 		field_label_description = course.description
@@ -29,12 +29,12 @@ class CourseTestClass(TestCase):
 		self.assertEquals(field_label_description, 'Test description')
 		self.assertEquals(field_label_logo, 'Test logo')
 
-	def test_contact(self):
+    def test_contact(self):
 		contact = Contact.objects.get(id = 1)
 		field_label_value = contact.value
 		self.assertEquals(field_label_value, 'Test value')
 
-	def test_branch(self):
+    def test_branch(self):
 		branch = Branch.objects.get(id = 1)
 		field_label_latitude = branch.latitude
 		field_label_longtitude = branch.longtitude
--- courses_app/tests/test_serializers.py
+++ courses_app/tests/test_serializers.py
@@ -7,33 +7,33 @@ from contact.models import Contact
 
 
 class CoursesSerializerTest(TestCase):
-		def setUp(cls):
-			category = Category.objects.create(name = "Test category", imgpath = "Test image")
+	def setUp(cls):
+		category = Category.objects.create(name = "Test category", imgpath = "Test image")
 			
-		def test_serializer_create(self):
-			serializer_data = {
-			        "name": "Test name",
-			        "description": "Test description",
-			        "logo": "Test logo",
-			        "category": 1,
-			        "contacts": [
-			        	{
-			        		"contact_type": 1,
-			        		"value": "Test value"
-			        	}
-			        ],
-					"branches": [
-			            {
-			                "latitude": "Test latitude",
-			                "longtitude": "Test longtitude",
-			                "adress": "Test adress"
-			            }
-			        ]
-			    }			
+	def test_serializer_create(self):
+		serializer_data = {
+			"name": "Test name",
+			"description": "Test description",
+			"logo": "Test logo",
+			"category": 1,
+			"contacts": [
+			        {
+			        	"contact_type": 1,
+			        	"value": "Test value"
+			        }
+			    ],
+			"branches": [
+			        {
+			            "latitude": "Test latitude",
+			            "longtitude": "Test longtitude",
+			            "adress": "Test adress"
+			        }
+			    ]
+			}			
 
-			serializer = serializers.CourseSerializer(data = serializer_data)
-			serializer.is_valid(raise_exception = True)
-			serializer.save()
+		serializer = serializers.CourseSerializer(data = serializer_data)
+		serializer.is_valid(raise_exception = True)
+		serializer.save()
 
 			
-			self.assertIsNotNone(Course.objects.get(id = 1))
+		self.assertIsNotNone(Course.objects.get(id = 1))
