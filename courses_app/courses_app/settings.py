85cfcf1a48fb254de75c09df5eef84993aaeebf5
Nazgul <ismailova_n@auca.kg>
2020-07-31 00:11:48 +0600

    deploy

--- courses_app/courses_app/settings.py
+++ courses_app/courses_app/settings.py
@@ -125,8 +125,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-import django_heroku
-django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
