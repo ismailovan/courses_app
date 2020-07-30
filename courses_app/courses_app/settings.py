c2363f5e1c134bd774c3fc38cdfd819bd28fe503
Nazgul <ismailova_n@auca.kg>
2020-07-30 23:54:43 +0600

    'deploy'

--- courses_app/courses_app/settings.py
+++ courses_app/courses_app/settings.py
@@ -125,6 +125,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+import django_heroku
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -133,3 +135,4 @@ STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
