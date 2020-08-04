e383d2e4f6747d0eac236fdb6b057660549b915c
Nazgul <ismailova_n@auca.kg>
2020-08-04 15:33:46 +0600

    'deploy'

--- courses_app/course/urls.py
+++ courses_app/course/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from course import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+
 urlpatterns = [
     path('', views.CourseList.as_view(), name = 'courseList'),
     path('<int:pk>/', views.CourseDetail.as_view(), name = 'courseDetail'),
